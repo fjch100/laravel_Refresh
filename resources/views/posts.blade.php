f01787d11d494d849d70e4370a15af3af5324098
Felix Centeno <fjch100@gmail.com>
2023-12-27 12:59:06 -0500

    first Commit, Initialize

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)    
+    <article>
+        <h1><a href="post/{{$post->slug}}">{{$post->title}}</a></h1>
+        <p>{!! $post->body !!}</p>
+    </article>
+    @endforeach
+@endsection
